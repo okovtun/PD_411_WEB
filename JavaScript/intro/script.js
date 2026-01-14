e004d8b199e14de319a3b5805aea09490af5b898
User <User@DESKTOP-QHG18FL>
2026-01-14 16:01:47 +0300

    Factorial DONE

---
+++ JavaScript/intro/script.js
@@ -0,0 +1,24 @@
+﻿// JavaScript source code
+function Factorial(n)
+{
+    let f = BigInt(1);
+    //document.write(typeof (f));
+    //document.write("<pre>");	//preformatted - учитываются проьелы и знаки переноса, а	так же используется моноширный шрифт.
+    for (let i = 1n; i <= n; i++)
+    {
+        f *= i;
+        //document.writeln(`${i}! = ${f};`);
+}
+			//document.write("</pre>");
+//Arithmetical overflow
+			return f;
+}
+function Factorial2()
+{
+    let source_input_field = document.getElementById("factorial-source");
+//alert("Привет!");
+    let source_value = source_input_field.value;
+    ////////////////////////////////
+    let factorial_result = document.getElementById("factorial-result");
+    factorial_result.innerHTML = Factorial(source_value);
+}
