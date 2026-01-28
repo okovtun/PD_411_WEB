367936d7d21e06bf575dc6a01b6bb8d7d329d27f
User <User@DESKTOP-QHG18FL>
2026-01-28 16:06:23 +0300

    Animation improved.

--- JavaScript/DOM/domanimation.js
+++ JavaScript/DOM/domanimation.js
@@ -1,26 +1,30 @@
 // JavaScript source code
-function Move()
-{
+var id = null;
+function Move() {
     const elem = document.getElementById("animation");
-    let x_pos = 0;
-    let y_pos = 0;
-    let x_shift = 1;
-    let y_shift = 1;
-    //let interval = document.getElementById("interval").value;
-    let id = setInterval(frame, 15);
-    function frame()
-    {
+    var x_pos = 0;
+    var y_pos = 0;
+    var x_shift = 1;
+    var y_shift = 1;
+    clearInterval(id);
+    //if (flag === true) {
+    //    clearInterval(id);
+    //    flag = true;
+    //}
+    let interval = document.getElementById("interval").value;
+    id = setInterval(frame, interval);
+    function frame() {
         //if (pos == 250) clearInterval(id);
         //else
         //{
-            x_pos += x_shift;
-            y_pos += y_shift;
-            elem.style.top = x_pos + 'px';
-            elem.style.left = y_pos + 'px';
-            if (x_pos === 0)      x_shift = 1;
-            if (x_pos === 350)    x_shift = -1;
-            if (y_pos === 0)      y_shift = 1;
-            if (y_pos === 250)    y_shift = -1;
+        x_pos += x_shift;
+        y_pos += y_shift;
+        elem.style.top = x_pos + 'px';
+        elem.style.left = y_pos + 'px';
+        if (x_pos === 0) x_shift = 1;
+        if (x_pos === 350) x_shift = -1;
+        if (y_pos === 0) y_shift = 1;
+        if (y_pos === 250) y_shift = -1;
 
         //}
     }
